c4e8cb0d3902897105c9597698c56d11fcc63e20
Jan Wieczorek <2095736w@student.gla.ac.uk>
2021-01-29 21:58:09 +0000

    Chapter 4 finished - 2 tests inexpicably result in failure

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -26,7 +26,6 @@ path('', views.index, name='index'),
 path('rango/', include('rango.urls')),
 # The above maps any URLs starting with rango/ to be handled by rango.
 path('admin/', admin.site.urls),
-static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
